1a8d0d4425c9a7034300faa17fa5daab896c9954
Brainbox11 <chibuzo.fg@gmail.com>
2023-06-06 15:44:08 +0100

    User token investments update

--- src/token/dto/sell-token.dto.ts
+++ src/token/dto/sell-token.dto.ts
@@ -1,15 +1,20 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsNumberString } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class SellTokenDto {
   @IsNumber()
+  @Type(() => Number)
   userId: number;
 
   @IsNumber()
+  @Type(() => Number)
   projectId: number;
 
   @IsNumber()
+  @Type(() => Number)
   amount: number;
 
   @IsNumber()
+  @Type(() => Number)
   tokenId: number;
 }
